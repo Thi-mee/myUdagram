f698411d6c658a4d79e5a647e0569c984103bf3b
Timilehin Adenuga <timswaggs1@outlook.com>
2022-08-27 20:43:18 +0100

    slight change

--- src/server.ts
+++ src/server.ts
@@ -33,7 +33,8 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   app.get('/filteredimage', async (req: Request, res: Response) => {
     const image_url = req.query.image_url.toString();
     if (!image_url) {
-      res.status(400).send('Image url is required');
+      res.status(400).send('Url of image required');
+      
     }
 
     const filtered_image = await filterImageFromURL(image_url);
